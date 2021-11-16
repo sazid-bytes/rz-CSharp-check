5b65203e55485ba447ca681a44799a13e274f4a6
Sazid <sazid.sunny@gmail.com>
2021-11-16 15:46:50 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rz_CSharp_check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
