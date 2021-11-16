7f957ff74116c43f4f6d2e3c4fea5b3eac9b0e22
Nazeeruddin <naseers.tech@gmail.com>
2021-11-16 15:54:31 +0530

    Apples Added check

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace rz_CSharp_check
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Apples");
         }
     }
 }
